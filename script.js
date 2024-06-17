7107efa5519730705c959bd4323c284c6b032190
ReeVool <126878419+ReeVool@users.noreply.github.com>
2024-06-17 07:46:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+//Gemini
+
+// // Получить все аудиофайлы в папке "аудио"
+// const audioFiles = document.querySelectorAll('audio[src*="audio/"]');
+
+// // Создать пустой массив для хранения путей к аудиофайлам
+// const audioPaths = [];
+
+// // Заполнить массив audioPaths путями к аудиофайлам
+// for (const audioFile of audioFiles) {
+//   audioPaths.push(audioFile.src);
+// }
+
+// // Получить кнопку
+// const button = document.getElementById('button');
+
+// // Обработчик события нажатия кнопки
+// button.addEventListener('click', () => {
+//   // Выбрать случайный путь к аудиофайлу из массива audioPaths
+//   const randomPath = audioPaths[Math.floor(Math.random() * audioPaths.length)];
+
+//   // Создать новый объект Audio
+//   const audio = new Audio(randomPath);
+
+//   // Воспроизвести аудио
+//   audio.play();
+// });
+
+
+
+//chatGPT
+
+// фотки ибры
+const ibra_left = document.getElementById('ibra_left');
+const ibra = document.getElementById('ibra');
+const ibra_right = document.getElementById('ibra_right');
+
+// клик по ибре
+document.getElementById('ibra').addEventListener('click', playRandomAudio);
+
+function playRandomAudio() {
+    const audioFiles = [
+        'audio/17 человек.opus',
+        'audio/анальные каналы.opus',
+        'audio/закрытые глаза.opus',
+        'audio/петушинные крики.opus',
+        'audio/русик нахуй пошел.opus',
+        'audio/уаааааа.opus',
+        'audio/ух пидорас.opus'
+    ];
+
+    const randomIndex = Math.floor(Math.random() * audioFiles.length);
+    const randomAudio = new Audio(audioFiles[randomIndex]);
+    randomAudio.play();
+}
+
+
+// клик по левой кнопке
+document.getElementById('left_btn').addEventListener('click', left_click);
+function left_click() {
+    const audio_zoebis = 'audio/зоебись.opus';
+    
+    const zoebis = new Audio(audio_zoebis);
+    zoebis.play();
+
+    ibra.style.display = 'none'
+    ibra_left.style.display = 'block';
+    setTimeout(() => {
+        ibra.style.display = 'block';
+        ibra_left.style.display = 'none';
+    }, 3000);
+}
+
+// клик по правой кнопке
+document.getElementById('right_btn').addEventListener('click', right_click);
+function right_click() {
+    const audio_shuyali = 'audio/схуяли.opus';
+    
+    const shuyali = new Audio(audio_shuyali);
+    shuyali.play();
+
+    const image = document.getElementById('right_img');
+    image.style.display = 'block';
+    ibra.style.display = 'none'
+    ibra_right.style.display = 'block';
+    setTimeout(() => {
+        image.style.display = 'none';
+        ibra.style.display = 'block';
+        ibra_right.style.display = 'none';
+    }, 2000);
+}
